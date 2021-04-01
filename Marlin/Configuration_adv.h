a41df6e10b0fa0b13ad15028f77f7b1af8c72ca4
Vince2373 <vincent.schroettle97@gmx.de>
2021-04-01 14:05:30 +0100

    Falsche Z Höhe eingegeben und Strom für Mechanical Gantry Levelling verringert

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3674,7 +3674,7 @@
  */
 #define MECHANICAL_GANTRY_CALIBRATION
 #if ENABLED(MECHANICAL_GANTRY_CALIBRATION)
-  #define GANTRY_CALIBRATION_CURRENT         400     // Default calibration current in ma
+  #define GANTRY_CALIBRATION_CURRENT         300     // Default calibration current in ma
   #define GANTRY_CALIBRATION_EXTRA_HEIGHT      15     // Extra distance in mm past Z_###_POS to move
   #define GANTRY_CALIBRATION_FEEDRATE         200     // Feedrate for correction move
   //#define GANTRY_CALIBRATION_TO_MIN                 // Enable to calibrate Z in the MIN direction
